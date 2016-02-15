a1350faed510cec5872d5be29fbcacee935958c7
anish <aniz_sthaa12@gmail.com>
2016-02-15 22:31:31 +0545

    first commit

---
+++ pattern1/Facade-Design-Pattern/FacadeDesignPattern/src/com/leapfrog/facadepattern/concreteclass/Circle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leapfrog.facadepattern.concreteclass;
+
+import com.leapfrog.facadepattern.Interface.Shape;
+
+/**
+ *
+ * @author User
+ */
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Circle is drawn");
+    }
+
+}
